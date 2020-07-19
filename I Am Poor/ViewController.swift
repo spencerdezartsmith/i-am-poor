65210a6fd4348161b9ff87c350e3346cb1723d22
Spencer Dezart-Smith <spencerdezartsmith@gmail.com>
2020-07-19 10:35:23 +1000

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Spencer Dezart-Smith on 19/7/20.
+//  Copyright © 2020 Spencer Dezart-Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
